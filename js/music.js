0ec04bc76022a1c8ee1637df235db640baf0f216
Skullface/Tarkov <106857740+terkoshi@users.noreply.github.com>
2023-02-20 19:52:16 -0500

    I'm lazy asf.

--- js/music.js
+++ js/music.js
@@ -1,10 +1,27 @@
-{
-    var audioBG = new Rythm()
-    
-    audioBG.setMusic('/assets/bg.mp3')
-    audioBG.setGain(1.2);
 
+var BG = document.getElementById('sequencer')
+var links = document.getElementById('links')
+
+links.style.display =  'none';
+BG.style.display =  'none';
+
+function play()
+{
+    var BG = document.getElementById('sequencer')
+    var links = document.getElementById('links')
+    var btn = document.getElementById('enter')
+
+    BG.style.display = 'inline';
+    links.style.display = 'inline';
+    enter.remove();
+
+        var audioBG = new Rythm()
+        
+        audioBG.setMusic('/assets/bg.mp3')
+        audioBG.setGain(0.1);
     
-    audioBG.start()
+        
+        audioBG.start()
+    
+       
 }
-   
